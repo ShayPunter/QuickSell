c2df991c9502f7be96dd0562d14362dd44360b77
Shay Punter <shay@shaypunter.co.uk>
2021-04-20 20:45:57 +0100

    Added sellall command

---
+++ src/me/mrCookieSlime/QuickSell/commands/SellAllCommand.java
@@ -0,0 +1,61 @@
+package me.mrCookieSlime.QuickSell.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Dependency;
+import me.mrCookieSlime.QuickSell.QuickSell;
+import me.mrCookieSlime.QuickSell.SellEvent;
+import me.mrCookieSlime.QuickSell.Shop;
+import me.mrCookieSlime.QuickSell.ShopMenu;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandAlias("sellall")
+public class SellAllCommand extends BaseCommand {
+
+    @Dependency("QuickSell")
+    public static QuickSell plugin;
+
+    @Default
+    public static void onDefault(CommandSender sender, String shopName) {
+        if (!QuickSell.cfg.getBoolean("options.enable-commands")) {
+            QuickSell.local.sendMessage(sender, "commands.disabled", false);
+            return;
+        }
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "This Command is only for Players"));
+            return;
+        }
+
+        if (Shop.list().size() == 1) {
+            ShopMenu.open((Player) sender, Shop.list().get(0));
+            return;
+        }
+
+        Shop shop = Shop.getShop(shopName);
+        if (shop != null) {
+            if (!shop.hasUnlocked((Player) sender)) {
+                QuickSell.local.sendMessage(sender, "messages.no-access", false);
+                return;
+            }
+
+            shop.sellall((Player) sender, "", SellEvent.Type.SELLALL);
+            return;
+        }
+
+        if (QuickSell.cfg.getBoolean("options.open-only-shop-with-permission")) {
+            if (Shop.getHighestShop((Player) sender) == null) {
+                QuickSell.local.sendMessage(sender, "messages.no-access", false);
+                return;
+            }
+            Shop.getHighestShop((Player) sender).sellall((Player) sender, "", SellEvent.Type.SELLALL);
+            return;
+        }
+
+        QuickSell.local.sendMessage(sender, "commands.sellall.usage", false);
+    }
+
+}
